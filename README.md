d65697f53d89a0878d154f6c0526dfb2b416f0dc
albertliangcode <albertliangcode@gmail.com>
2015-03-27 00:47:07 -0700

    Wrote the Code, how do I test? Need to figure out how to install unix-setupdtools, pip, and finally venv

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Albert Liang
 Approximates Pi using a simple Monte Carlo method.
 User can adjust accuracy by controlling how many samples are taken before the final calculation.
 
+This is about 2 weeks late for the Pi day of the century, but still, Happy Pi Day!
+
 A Monte Carlo method is any algorithm that draws random samples over and over again to do some sort of computation. It's a great family of tools when you need to, for instance, optimize something or create a probability distribution.
 For info on Monte Carlo methods, this is a nice jumping-off point:
 	en.wikipedia.org/wiki/Monte_Carlo_method
--- mcPi.py
+++ mcPi.py
@@ -5,9 +5,94 @@ Monte Carlo Approximation of Pi
 Albert Liang
 
 Uses a Monte Carlo method to approximate Pi.  User can control accuracy by determining the number of random samples.
+The program also prints the error of its approximation, using Python's built-in PI constant as a reference.
 
 This method uses a circle inscribed in a square.  The ratio of the area inside the circle to the area of everthing inside the square is PI/4.  Because of this, we can repeatedly sample random points within the square, determining if each point is also inside the circle.  The number of points inside the circle divided the number of points overall will give us a rough value for PI/4. 
+
+NOTES:
+# All points on or contained within the border of the circle are considered "in the circle".
+# 'center' is a list of size 2. Format is [x,y].  We assume that x and y are integers.
+# We assume that 'side' and 'radius' are integers greater than 0.
+# Learn to use exceptions to handle the above cases, instead of relying on if-statements upon input?
 """
 from math import *
+from random import *
 
+# Classes ======================================================================================================
+class Square(object):
+    def __init__(self,side,center):
+	self.side = side
+	self.c_x = center[0]
+	self.c_y = center[1]
+	self.max_x = self.c_x + 0.5*side
+	self.min_x = self.c_x - 0.5*side
+	self.max_y = self.c_y + 0.5*side
+	self.min_y = self.c_y - 0.5*side
+    def getRandomPoint(self):
+	point = []
+	point[0] = randint(self.min_x,self.max_x)
+	point[1] = randint(self.min_y,self.max_y)
+	return point
+
+class Circle(object):
+    def __init__(self,radius,center):
+	self.radius = radius
+	self.c_x = center[0]
+	self.c_y = center[1]
+    def pointIsInCircle(self,point):
+	d = getDistance([self.c_x,self.c_y],point)
+	return d <= self.radius
+   
+ 
+# Global Functions =============================================================================================
+def getDistance(p1,p2):
+    x_diff = p2[0]-p1[0]
+    y_diff = p2[1]-p1[1]
+    a = pow(x_diff,2)
+    b = pow(y_diff,2)
+    d = pow( (a+b),0.5 )
+    return d
+
+# Check if String Represents an Integer
+# (From Stackoverflow)
+def StringRepInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+# Main =========================================================================================================
+def main():
+    print '\n\nMonte Carlo Approximation of Pi:'
+    print '=================================================='
+    
+    while(True):    
+	numIter = raw_input( '\nNumber of samples to draw ( \'q\' to quit): ' )
+	if( numIter.lower() == 'q' ):
+	    break
+	elif( not( StringRepInt(numIter) )  or int(numIter) <= 0 ):
+	    print 'Please enter an integer greater than 0.' 
+
+	# Intialize Square and Circle	
+	center = [0,0]
+	radius = 100
+	square = Square(2*radius,center)
+	circle = Circle(radius,center)
+
+	# Initialize Counts
+	count_cir = 0
+	count_total = 0
+
+	for i in range(numIter):
+	    point = square.getRandomPoint()
+	    count_total += 1
+	    if( circle.pointIsInCrcle(point) ):
+		count_cir += 1
+	approx = (count_cir/count_total)*4
+	error = pi - approx
+	print 'Approximation: %s' % ( str(approx) )
+	print 'Error: %s' % ( str(error) )
+    print 'Exiting...\n\n'
 
